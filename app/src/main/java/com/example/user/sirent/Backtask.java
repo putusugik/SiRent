82a24e74c34047ecacc6b02e4470e24b23fb579d
I Putu Sugi Almantara <sugi.almantara@yahoo.co.id>
2016-11-05 17:28:22 +0800

    Go go go

---
+++ app/src/main/java/com/example/user/sirent/Backtask.java
@@ -0,0 +1,30 @@
+package com.example.user.sirent;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by User on 11/5/2016.
+ */
+
+public class Backtask extends AsyncTask<String, Void, Void> {
+    @Override
+    protected Void doInBackground(String... params) {
+        return null;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+    }
+
+    @Override
+    protected void onProgressUpdate(Void... values) {
+        super.onProgressUpdate(values);
+    }
+
+}
