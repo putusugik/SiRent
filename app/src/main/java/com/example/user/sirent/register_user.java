29d851fdb7e113d0e268f958b27a5e71ca38dcc1
I Putu Sugi Almantara <sugi.almantara@yahoo.co.id>
2016-11-08 23:27:16 +0800

    Go go go

--- app/src/main/java/com/example/user/sirent/register_user.java
+++ app/src/main/java/com/example/user/sirent/register_user.java
@@ -4,13 +4,15 @@ import android.app.AlertDialog;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class register_user extends AppCompatActivity {
     EditText namadep, namabel, email, username, password, repassword;
-
+    String namad, namab, mail, user, pass, repass;
+    private static final String TAG = "your activity name";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,11 +23,40 @@ public class register_user extends AppCompatActivity {
         username = (EditText) findViewById(R.id.username);
         password = (EditText) findViewById(R.id.password);
         repassword = (EditText) findViewById(R.id.repassword);
+        namad = namadep.getText().toString();
+        namab = namabel.getText().toString();
+        mail = email.getText().toString();
+        user = username.getText().toString();
+        pass = password.getText().toString();
+        repass = repassword.getText().toString();
+
 
     }
 
     public void onregister(View view) {
-        if (namadep.getText().toString() != null && namabel.getText().toString() != null && email.getText().toString() != null
+        if (pass != null || !pass.equals("") && repass !=  null || !repass.equals("")){
+
+                AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+                alertDialog.setTitle("Warning");
+                alertDialog.setMessage("Password sama!");
+                alertDialog.show();
+
+        } else  {
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+            alertDialog.setTitle("Warning");
+            alertDialog.setMessage("Password harus diisi!");
+            alertDialog.show();
+        }
+        }
+
+
+
+        /*if (password.getText().toString().equals(null) && repassword.getText().toString().equals(null)) {
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+            alertDialog.setTitle("Warning");
+            alertDialog.setMessage("Password harus diisi!");
+            alertDialog.show();
+        } else if (namadep.getText().toString() != null && namabel.getText().toString() != null && email.getText().toString() != null
                 && username.getText().toString() != null && password.getText().toString() != null && repassword.getText().toString() != null) {
             if (password.getText().toString().equals(repassword.getText().toString())) {
                 String nama_dep = namadep.getText().toString();
@@ -36,14 +67,140 @@ public class register_user extends AppCompatActivity {
                 String type = "register";
                 Backtask backtask = new Backtask(this);
                 backtask.execute(type, nama_dep, nama_bel, e_mail, user, pass);
-
-
             } else {
                 AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
                 alertDialog.setTitle("Warning");
                 alertDialog.setMessage("Password tidak sama!");
                 alertDialog.show();
+
             }
         }
+        else if (namadep.getText().toString().equals(null)){
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+            alertDialog.setTitle("Warning");
+            alertDialog.setMessage("Nama Depan masih kosong");
+            alertDialog.show();
+        }
+        else if (namabel.getText().toString().equals(null)){
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+            alertDialog.setTitle("Warning");
+            alertDialog.setMessage("Nama Belakang masih kosong");
+            alertDialog.show();
+        }
+        else if (email.getText().toString().equals(null)){
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+            alertDialog.setTitle("Warning");
+            alertDialog.setMessage("Email masih kosong");
+            alertDialog.show();
+        }
+        else if (username.getText().toString().equals(null)){
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+            alertDialog.setTitle("Warning");
+            alertDialog.setMessage("Username masih kosong");
+            alertDialog.show();
+        }
+        else if (password.getText().toString().equals(null)){
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+            alertDialog.setTitle("Warning");
+            alertDialog.setMessage("Password masih kosong");
+            alertDialog.show();
+        }
+        else if (repassword.getText().toString().equals(null)){
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+            alertDialog.setTitle("Warning");
+            alertDialog.setMessage("Ulangi Password masih kosong");
+            alertDialog.show();
+        }
+        else if (namadep.getText().toString().equals(null) && namabel.getText().toString().equals(null)){
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+            alertDialog.setTitle("Warning");
+            alertDialog.setMessage("Nama Depan dan Nama Belakang masih kosong");
+            alertDialog.show();
+        }
+        else if (namadep.getText().toString().equals(null) && email.getText().toString().equals(null)){
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+            alertDialog.setTitle("Warning");
+            alertDialog.setMessage("Nama Depan dan Email masih kosong");
+            alertDialog.show();
+        }
+        else if (namadep.getText().toString().equals(null) && username.getText().toString().equals(null)){
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+            alertDialog.setTitle("Warning");
+            alertDialog.setMessage("Nama Depan dan Username masih kosong");
+            alertDialog.show();
+        }
+        else if (namadep.getText().toString().equals(null) && password.getText().toString().equals(null)){
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+            alertDialog.setTitle("Warning");
+            alertDialog.setMessage("Nama Depan dan Password masih kosong");
+            alertDialog.show();
+        }
+        else if (namadep.getText().toString().equals(null) && repassword.getText().toString().equals(null)){
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+            alertDialog.setTitle("Warning");
+            alertDialog.setMessage("Nama Depan dan Password masih kosong");
+            alertDialog.show();
+        }
+        else if (namabel.getText().toString().equals(null) && email.getText().toString().equals(null)){
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+            alertDialog.setTitle("Warning");
+            alertDialog.setMessage("Nama Belakang dan Email masih kosong");
+            alertDialog.show();
+        }
+        else if (namabel.getText().toString().equals(null) && username.getText().toString().equals(null)){
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+            alertDialog.setTitle("Warning");
+            alertDialog.setMessage("Nama Belakang dan Username masih kosong");
+            alertDialog.show();
+        }
+        else if (namabel.getText().toString().equals(null) && password.getText().toString().equals(null)){
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+            alertDialog.setTitle("Warning");
+            alertDialog.setMessage("Nama Belakang dan Password masih kosong");
+            alertDialog.show();
+        }
+        else if (namabel.getText().toString().equals(null) && repassword.getText().toString().equals(null)){
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+            alertDialog.setTitle("Warning");
+            alertDialog.setMessage("Nama Belakang dan Password masih kosong");
+            alertDialog.show();
+        }
+        else if (email.getText().toString().equals(null) && username.getText().toString().equals(null)){
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+            alertDialog.setTitle("Warning");
+            alertDialog.setMessage("Email dan Username masih kosong");
+            alertDialog.show();
+        }
+        else if (email.getText().toString().equals(null) && password.getText().toString().equals(null)){
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+            alertDialog.setTitle("Warning");
+            alertDialog.setMessage("Email dan Password masih kosong");
+            alertDialog.show();
+        }
+        else if (email.getText().toString().equals(null) && repassword.getText().toString().equals(null)){
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+            alertDialog.setTitle("Warning");
+            alertDialog.setMessage("Email dan Password masih kosong");
+            alertDialog.show();
+        }
+        else if (username.getText().toString().equals(null) && password.getText().toString().equals(null)){
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+            alertDialog.setTitle("Warning");
+            alertDialog.setMessage("Username dan Password masih kosong");
+            alertDialog.show();
+        }
+        else if (username.getText().toString().equals(null) && repassword.getText().toString().equals(null)){
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+            alertDialog.setTitle("Warning");
+            alertDialog.setMessage("Username dan Password masih kosong");
+            alertDialog.show();
+        }
+        else {
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+            alertDialog.setTitle("Warning");
+            alertDialog.setMessage("Semua form wajib diisi!");
+            alertDialog.show();
+            namadep.requestFocus();
+        }*/
     }
-}
+
