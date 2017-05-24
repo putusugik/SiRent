4a5bea58ed52a1f33d33649a63fa7e9aba61f894
Sugik <sugi.almantara@yahoo.co.id>
2017-05-24 14:59:22 +0800

    Sing ade ape dee (Onprogress status transaksi user)

--- register_user.php
+++ register_user.php
@@ -6,46 +6,85 @@
 	$nama_depan = $_POST["nama_depan"];
 	$nama_belakang = $_POST["nama_belakang"];
 	$email = $_POST["email"];
-	$qry1 = "insert into tb_user (nama_user, user_pass, user_registered) values ('$user_name', '$user_pass', now());";
-	$res1 = mysqli_query($link, $qry1);
-	$qrytmp = "CALL random_code_verify();";
-	$restm mysqli_query($link, $qrytmp);
-	if($res1){
-		$lastid = mysqli_insert_id($link);
-		$qry2 = "insert into tb_profiluser (nama_depan, nama_belakang, email, ID_user) values ('$nama_depan', '$nama_belakang', '$email', '$lastid');";
-		$res2 = mysqli_query($link, $qry2);
-			if($res2) {
-				$respon["sukses"]=1;
-				$respon["pesan"] = "Sukses";
-				smptmailer($email);
-				echo json_encode($respon);
-			}//echo json_encode($user);
+	
+	$qry = "select email from tb_profiluser where email = '$email';";
+	$res = mysqli_query($link, $qry);
+	if(mysqli_num_rows($res)==0){
+		$qry1 = "insert into tb_user (nama_user, user_pass, user_registered) values ('$user_name', '$user_pass', now());";
+		$res1 = mysqli_query($link, $qry1);
+		
+		if($res1){
+			$lastid = mysqli_insert_id($link);
+			
+			$qry12 = "UPDATE tb_user SET ver_code=concat(
+				  substring('ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789', rand(@seed:=round(rand('$lastid')*4294967296))*36+1, 1),
+				  substring('ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789', rand(@seed:=round(rand(@seed)*4294967296))*36+1, 1),
+				  substring('ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789', rand(@seed:=round(rand(@seed)*4294967296))*36+1, 1),
+				  substring('ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789', rand(@seed:=round(rand(@seed)*4294967296))*36+1, 1),
+				  substring('ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789', rand(@seed:=round(rand(@seed)*4294967296))*36+1, 1),
+				  substring('ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789', rand(@seed:=round(rand(@seed)*4294967296))*36+1, 1),
+				  substring('ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789', rand(@seed:=round(rand(@seed)*4294967296))*36+1, 1),
+				  substring('ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789', rand(@seed)*36+1, 1)
+				 )
+				WHERE ID='$lastid';";
+			$res3 = mysqli_query ($link, $qry12);
+			$qry2 = "insert into tb_profiluser (nama_depan, nama_belakang, email, ID_user) values ('$nama_depan', '$nama_belakang', '$email', '$lastid');";
+			$res2 = mysqli_query($link, $qry2);
+				if($res2) {
+					$respon["sukses"]=1;
+					$respon["pesan"] = "Sukses";
+					sendCode ($email, $link);
+					echo json_encode($respon);
+				}//echo json_encode($user);
+		} else {
+			$respon["sukses"]=0;
+			$respon["pesan"] = "Invalid";
+			echo json_encode($respon);
+		}
 	} else {
-		$respon["sukses"]=0;
-		$respon["pesan"] = "Invalid";
-		echo json_encode($respon);
+			$respon["sukses"]=2;
+			$respon["pesan"] = "Email sudah terdaftar";
+			echo json_encode($respon);
 	}
-	$link->close();
+	
+	
+	//$link->close();
+	
+	
+	function sendCode ($mail, $link){
+		$res3 = mysqli_query($link, "SELECT tb_user.`ID`, tb_user.`ver_code`, tb_profiluser.`email` 
+							FROM tb_user 
+							INNER JOIN tb_profiluser ON tb_user.`ID`=tb_profiluser.`ID_user`
+							WHERE email='$mail';");
+		$row2=mysqli_fetch_array($res3);
+		$id_login=$row2['ID'];
+		$validation_code=$row2['ver_code'];
+		$body="Silakan klik link dibawah untuk melakukan validasi akun. 
+		<br> <a href= 'http://sirent.esy.es/verification.php?id=$id_login&code=$validation_code'> Klik </a>";
+		smtpmailer($mail, $body);
+	}
 	
 	function smtpmailer($to, $body) { 
 		global $error;
 		require("class.phpmailer.php");
 		$mail = new PHPMailer();  // create a new object
 		$mail->IsSMTP(); // enable SMTP
-		$mail->SMTPDebug = 0;  // debugging: 1 = errors and messages, 2 = messages only
+		$mail->SMTPDebug = 1;  // debugging: 1 = errors and messages, 2 = messages only
 		$mail->SMTPAuth = true;  // authentication enabled
-		$mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for GMail
+		$mail->SMTPSecure = 'tls'; // secure transfer enabled REQUIRED for GMail
 		$mail->Host = 'smtp.gmail.com';
-		$mail->Port = 465; 
-		$mail->Username = 'e.ticketing.kereta.api@gmail.com';  
-		$mail->Password = 'ogerganteng';           
-		$mail->SetFrom('e.ticketing.kereta.api@gmail.com', 'Sistem Informasi Kereta Api');
+		$mail->Port = 587; 
+		$mail->Priority = 1;
+		$mail->Username = 'ta.sirent@gmail.com';  
+		$mail->Password = 'sugiksirent';           
+		$mail->SetFrom('ta.sirent@gmail.com', 'Sistem Informasi Rental Kendaraan');
 		$mail->Subject = 'Validasi Akun';
 		$mail->Body =  $body;
 		$mail->IsHTML(true);
 		$mail->AddAddress($to);
 		if(!$mail->Send()) {
 			$error = 'Mail error: '.$mail->ErrorInfo; 
+			echo $error;
 			return false;
 		} else {
 			$error = 'Message sent!';
